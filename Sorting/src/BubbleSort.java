2efb6e7d3a26de2a9f43b7e9f41222dfff16a54c
Rajbabu67 <kingrajbabu6@gmail.com>
2024-03-08 13:14:04 +0530

    formatted

--- Sorting/src/BubbleSort.java
+++ Sorting/src/BubbleSort.java
@@ -2,38 +2,36 @@
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-		
+		int[] arr = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
 		System.out.println("Before Sorting ....");
-		
-		for(int num : arr) {
+
+		for (int num : arr) {
 			System.out.print(num + " ");
 		}
-		
+
 		System.out.println();
-		
+
 		arr = bubbleSort(arr);
-		
-		
-		
+
 		System.out.println("After Sorting ....");
-		
-		for(int num : arr) {
+
+		for (int num : arr) {
 			System.out.print(num + " ");
 		}
 	}
 
 	private static int[] bubbleSort(int[] arr) {
 		int temp = 0;
-		
-		for(int i=0; i<arr.length; i++) {
-			
-			for(int j=0; j<arr.length-i-1; j++) {
-				
-				if(arr[j] > arr[j+1]) {
+
+		for (int i = 0; i < arr.length; i++) {
+
+			for (int j = 0; j < arr.length - i - 1; j++) {
+
+				if (arr[j] > arr[j + 1]) {
 					temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
 				}
 			}
 		}
