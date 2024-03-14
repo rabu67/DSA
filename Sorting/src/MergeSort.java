b76ff736d720c920206adf8485f15ba5c10234f6
rabu67 <rajbabumaruthu@gmail.com>
2024-03-14 10:32:47 +0530

    Formatted

--- Sorting/src/MergeSort.java
+++ Sorting/src/MergeSort.java
@@ -4,65 +4,64 @@ public class MergeSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Random rand = new Random();	
-		
+		Random rand = new Random();
+
 		int[] inputArr = new int[10];
-		
-		for(int i=0; i<inputArr.length; i++) {
+
+		for (int i = 0; i < inputArr.length; i++) {
 			inputArr[i] = rand.nextInt(10000);
 		}
-		
+
 		printArrays(inputArr);
-		
+
 		mergeSort(inputArr);
-		
+
 		printArrays(inputArr);
 		System.out.println("Done");
 	}
 
 	private static void mergeSort(int[] inputArr) {
-		
-		
+
 		int n = inputArr.length;
-		
-		if(n < 2) {
+
+		if (n < 2) {
 			return;
 		}
-		
-		int mid = n/2;
-		
+
+		int mid = n / 2;
+
 		int[] leftArr = new int[mid];
-		
-		int[] rightArr = new int[n-mid];
-		
-		for(int i=0;i<mid;i++) {
+
+		int[] rightArr = new int[n - mid];
+
+		for (int i = 0; i < mid; i++) {
 			leftArr[i] = inputArr[i];
 		}
-		
-		for(int i=mid;i<n;i++) {
-			rightArr[i-mid] = inputArr[i];
+
+		for (int i = mid; i < n; i++) {
+			rightArr[i - mid] = inputArr[i];
 		}
-		
+
 //		printArrays(leftArr);
 //		printArrays(rightArr);
-		
+
 		mergeSort(leftArr);
 		mergeSort(rightArr);
-		
+
 		merge(inputArr, leftArr, rightArr);
 //		System.out.println("end dividing");
-		
+
 	}
-	
+
 	private static void merge(int[] inputArr, int[] leftArr, int[] rightArr) {
-		
+
 		int leftHalf = leftArr.length;
 		int rightHalf = rightArr.length;
-		
+
 		int i = 0, j = 0, k = 0;
-		
-		while(i < leftHalf && j < rightHalf) {
-			if(leftArr[i] < rightArr[j]) {
+
+		while (i < leftHalf && j < rightHalf) {
+			if (leftArr[i] < rightArr[j]) {
 				inputArr[k] = leftArr[i];
 				i++;
 			} else {
@@ -71,23 +70,23 @@ public class MergeSort {
 			}
 			k++;
 		}
-		
-		while(i < leftHalf) {
+
+		while (i < leftHalf) {
 			inputArr[k] = leftArr[i];
 			i++;
 			k++;
 		}
-		while(j < rightHalf) {
+		while (j < rightHalf) {
 			inputArr[k] = rightArr[j];
 			j++;
 			k++;
 		}
-		
+
 	}
 
 	private static void printArrays(int[] inputArr) {
 		System.out.println();
-		for(int arr: inputArr) {
+		for (int arr : inputArr) {
 			System.out.print(arr + " ");
 		}
 		System.out.println();
